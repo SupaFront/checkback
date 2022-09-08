e30385a2b47e6fb7028547c6b2b6006c5ba62cb2
Yehor Podmazko <bluewotka@gmail.com>
2022-09-08 09:51:08 +0300

    lolkek

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
-  host: "https://buiapipps.herokuapp.com/",
-  port: "",
+  host: "buiapipps.herokuapp.com",
+  port: "443",
   app: {
     keys: env.array("APP_KEYS"),
   },
